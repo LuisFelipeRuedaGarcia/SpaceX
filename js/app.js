7868170f61f3fcc857e31b36636c085790bd06af
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-04-25 16:42:39 -0500

    feat: :ambulance: videos tardan en cargar en Chrome
    Se insertan las tarjetas y sus respesctivos modales funcionales en el html, pero en chrome los videos a veces tardan demasiado en aparecer en el modal, se limita el numero de cards a insertar y con esto la cantidad de videos a cargar a solo 4, en Mozilla cargan casi al instante chorme sigue igual, la pagina se queda cargando.

--- js/app.js
+++ js/app.js
@@ -8,18 +8,37 @@ fetch(url)
 }
 
 function insertCards(launches){
-    launches.forEach(launche => {
-        console.log(divCards);
+    let count = -1
+    for (let i = 0; i < 4; i++) {
+        count++;
         divCards.insertAdjacentHTML("beforeend",`
         <div class="card">
-            <img class="maid-img-top" src="${launche.links.mission_patch}" alt="Card image cap">
+            <img class="maid-img-top" src="${launches[i].links.mission_patch}" alt="Card image cap">
             <div class="card-body">
-                <p class="card-text"><span>${launche.launch_year}</span></p>
-                <a href="#" class="btn btn-primary">Details</a>
+            <h5 class="card-text"><span>${launches[i].mission_name}</span></h5>
+                <p class="card-text"><span>${launches[i].launch_year}</span></p>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#miModal${count}">Details</button>
+            </div>
+        </div>
+        <div class="modal fade" id="miModal${count}" tabindex="-1" aria-hidden="true" aria-labelledby="modalTitle">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-tittle" id="modalTitle">Information ${launches[i].mission_name}</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                    <iframe width="100%" height="270" src="https://www.youtube.com/embed/${launches[i].links.youtube_id}" allowfullscreen></iframe>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary"data-bs-dismiss="modal"> Cerrar</button>
+                        <button type="button" class="btn btn-primary">Aceptar</button>
+                    </div>
+                </div>
             </div>
         </div>
         `)
 
-    });
+    };
 }
 principal();
